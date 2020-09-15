07ed216b8690bddd35005d8435566e57514cd64d
leoovillalta <leonardovillalta2511@gmail.com>
2020-09-14 23:06:56 -0600

    Gravity applied

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
     [SerializeField]
     private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,21 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * _speed;
         //MOVE based on direction
+
+        //if grounded
+        //do nothing
+        //else
+        //appply gravity
+        if(_controller.isGrounded == true)
+        {
+            //do nothing maybe jump later
+        }
+        else
+        {
+            velocity.y -= _gravity;
+        }
+
+
         _controller.Move(velocity * Time.deltaTime);
     }
 }
